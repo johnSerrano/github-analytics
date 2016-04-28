e425cdd867393598e6d91b246d95a4db0a8227f2
John Serrano <johnserrano404@gmail.com>
2016-04-27 21:08:27 -0700

    get ALL of a user's repos

--- src/parsers.rs
+++ src/parsers.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use hyper::Client;
 use hyper::header::UserAgent;
+use hyper::Url;
 use std::io::prelude::*;
 use json_structs::{User, Repo};
 use rustc_serialize::json;
@@ -111,21 +112,34 @@ pub fn get_langs_for_user(all_langs: HashMap<String, HashMap<String, i64>>) -> H
 // Get all repos from a user. Returns vector of Repo structs, each containing
 // info on one repo and api links to more info on that repo.
 pub fn get_repos(user: User) -> Vec<Repo> {
-    let client = Client::new();
-    let mut resp = client.get(user.repos_url.unwrap().as_str())
-                         .header(UserAgent("rust-hyper".to_string()))
-                         .header(Authorization("token f4f6caa6a3e5f5da491e42d6ee14708f325ad655"
-                                                   .to_string()))
-                         .send()
-                         .unwrap();
-    let mut response_content = String::new();
-    match resp.read_to_string(&mut response_content) {
-        Ok(_) => {
+
+    let mut repos: Vec<Repo> = Vec::new();
+    let iterations = (user.public_repos / 100) + 1;
+
+    for page in 0..iterations {
+        let mut url = Url::parse(user.repos_url.clone().unwrap().as_str()).unwrap();
+        let page_num = page + 1;
+        let query_string = format!("per_page=100&page={}", page_num);
+        let query_option = Some(query_string.as_str());
+        url.set_query(query_option);
+
+        let client = Client::new();
+        let mut resp = client.get(url)
+                             .header(UserAgent("rust-hyper".to_string()))
+                             .header(Authorization("token f4f6caa6a3e5f5da491e42d6ee14708f325ad655"
+                                                       .to_string()))
+                             .send()
+                             .unwrap();
+        let mut response_content = String::new();
+        match resp.read_to_string(&mut response_content) {
+            Ok(_) => {
             ;
+            }
+            Err(err) => panic!("{}", err),
         }
-        Err(err) => panic!("{}", err),
+        let mut iteration_repos: Vec<Repo> = json::decode(&*response_content).unwrap();
+        repos.append(&mut iteration_repos);
     }
-    let repos: Vec<Repo> = json::decode(&*response_content).unwrap();
     return repos;
 }
 
